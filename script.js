27609b098829fe90ead869dfaf795b000ce71a8b
BenyamWorku <benworku@gmail.com>
2021-03-20 00:13:47 +0000

    first iteration

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  
+  
+  episodeList.forEach((episode) => {
+    const { name, image, season,number,
+      summary } = episode;
+    const episodeEl = document.createElement('div');
+    episodeEl.classList.add("episode-card");
+    episodeEl.innerHTML = `
+      <div class="season-title">
+          <h1>S${season}E${number}</h1>
+          <h2>${name}</h2>
+      </div>
+      <img src="${image.medium}" alt="${name}">
+      <div class="summary-text">
+        <h3>${summary}
+        </h3>
+      </div>`
+
+    rootElem.appendChild(episodeEl);
+
+  })
+  
 }
 
 window.onload = setup;
